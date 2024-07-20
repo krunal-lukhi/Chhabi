10a2de6e3bfc5c1624fefeb85cc73d1a05c6244b
krunal-lukhi <kdlukhi2002@gmail.com>
2024-07-21 00:55:13 +0530

    init

---
+++ src/main/java/com/gj4/chhabi/ChhabiApplication.java
@@ -0,0 +1,13 @@
+package com.gj4.chhabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChhabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChhabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gj4/chhabi/ChhabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gj4.chhabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChhabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
